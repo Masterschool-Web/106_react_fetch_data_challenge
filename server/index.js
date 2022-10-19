0997c736d9a57ccd1ff6dbee385217ba0c4b1d8a
David Rajcher <davidrajcher@Davids-MacBook-Pro-2.local>
2022-10-19 18:09:26 +0200

    created a server

--- server/index.js
+++ server/index.js
@@ -1,31 +1,120 @@
 const express = require("express");
 const products = require("./data/products.json");
+const categories = require("./data/categories.json");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
 // GET all products
 app.get("/products", (req, res) => {
+  const { search, brand, max, min, rating } = req.query;
+
+  if (!search && !brand && !max && !min && !rating) {
+    return res.status(200).send({
+      length: products.length,
+      products: products.map((product) => {
+        const { id, title, price, discountPercentage, thumbnail } = product;
+        return {
+          id,
+          title,
+          priceBeforeDiscount: price,
+          discountPercentage,
+          thumbnail,
+        };
+      }),
+    });
+  }
+
+  if (max && isNaN(max)) {
+    return res.status(400).send({
+      error: "Max query has to be a number",
+    });
+  }
+
+  if (min && isNaN(min)) {
+    return res.status(400).send({
+      error: "Min query has to be a number",
+    });
+  }
+
+  if (rating && isNaN(rating)) {
+    return res.status(400).send({
+      error: "Rating query has to be a number",
+    });
+  }
+
+  if (!search || search.length < 3) {
+    return res.status(400).send({
+      error: "search query has to be longer than 3 characters",
+    });
+  }
+
+  const productsBySearchWord = products.filter((product) => {
+    const isBrand =
+      !brand || product.brand.toLowerCase() === brand.toLowerCase();
+    const isSearch =
+      !search || product.title.toLowerCase().includes(search.toLowerCase());
+    const isMax = !max || product.price < parseInt(max);
+    const isMin = !min || parseInt(min) < product.price;
+    const isRating = !rating || product.rating < parseFloat(rating);
+
+    return isBrand && isSearch && isMax && isMin && isRating;
+  });
+
+  if (productsBySearchWord.length === 0) {
+    return res.status(404).send({
+      error: `No product was find`,
+    });
+  }
+
   res.status(200).send({
-    length: products.length,
-    products,
+    length: productsBySearchWord.length,
+    products: productsBySearchWord,
   });
 });
 
 // GET all categories
+app.get("/categories", (req, res) => {
+  res.status(200).send({
+    categories,
+  });
+});
 
 // GET category products
+app.get("/products/:category", (req, res) => {
+  let { category } = req.params;
+  category = category.toLowerCase();
+
+  const productsByCategory = products.filter(
+    (product) => product.category === category
+  );
+
+  res.status(200).send({
+    length: productsByCategory.length,
+    products: productsByCategory,
+  });
+});
 
 // GET full data for a product
+app.get("/product/:id", (req, res) => {
+  let { id } = req.params;
+  id = parseInt(id);
 
-// GET products by search field
+  const product = products.find((product) => product.id === id);
 
-// BONUS
+  if (!product) {
+    return res.status(404).send({
+      error: "No product was found",
+    });
+  }
 
-// GET all products by brand
-
-// GET all products by prices
-
-// GET all products by rating
+  res.status(200).send({
+    product,
+  });
+});
 
 app.listen(4000, () => {
   console.log("server running on port", 4000);
